42ee805d598c7ab858bd17d698d5c43179467d3a
iamdevopsdevloper <devops@gmail.com>
2022-09-09 20:42:16 +0530

    Testing the code

---
+++ GitTest.java
@@ -0,0 +1,11 @@
+package mycompany;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("This is final test");
+		System.out.println("ok........");
+
+	}
+
+}
